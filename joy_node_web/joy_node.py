6a0dfd351f5b9cbceb81ea687200516a21dd12ff
sanjofumihiro <sanjofumihiro@gmail.com>
2024-06-22 21:58:45 +0900

    ええ感じにURI都合つけたで

--- joy_node_web/joy_node.py
+++ joy_node_web/joy_node.py
@@ -11,6 +11,7 @@ import threading
 
 app = FastAPI()
 msg = Joy()
+msg2 = Joy()
 @app.get("/joy")
 async def get():
     htm = """
@@ -31,6 +32,8 @@ async def get():
 </head>
 <body></body>
 <script>
+const uri_obj = new URL(window.location.href);
+const domain = uri_obj.host
 const pad_info = {
     id:"unknown",
     buttons:[],
@@ -74,20 +77,20 @@ window.addEventListener("gamepaddisconnected", (e)=>{
     });
 
 function webSocketInit(){
-    const ws = new WebSocket("ws://127.0.0.1:8700/joy");
+    const ws = new WebSocket("ws://"+domain+"/joys");
     ws.onopen = ()=>{
         status.node_connect = true;
         status.ws = ws;
-        document.body.innerHTML += "Open ws://127.0.0.1:8700/joy<br>";
+        document.body.innerHTML += "Open ws://"+domain+"/joy<br>";
     };
     ws.onclose = ()=>{
         status.node_connect = false;
-        document.body.innerHTML += "Close ws://127.0.0.1:8700/joy<br>";
+        document.body.innerHTML += "Close ws://"+domain+"/joy<br>";
         status.trying = false;
     }
     ws.onerror = (err) =>{
         console.log("Websocket error!! ", err);
-        document.body.innerHTML += "Error ws://127.0.0.1:8700/joy<br>";
+        document.body.innerHTML += "Error ws://"+domain+"/joy<br>";
     }
     status.trying = true;
 }
@@ -96,7 +99,7 @@ function retryWebsocket(){
     if (status.node_connect || status.trying)
         return;
     webSocketInit();
-    document.body.innerHTML += "Retrying ws://127.0.0.1:8700/joy<br>";
+    document.body.innerHTML += "Retrying ws://"+domain+"/joy<br>";
 
 }
 
@@ -109,7 +112,7 @@ webSocketInit();
     """
     return HTMLResponse(htm)
     
-@app.websocket("/joy")
+@app.websocket("/joys")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     global msg
@@ -127,10 +130,28 @@ async def websocket_endpoint(websocket: WebSocket):
                 msg.buttons.append(int(gamepad_info["buttons"][i]))
             else:
                 msg.buttons[i] = int(gamepad_info["buttons"][i])
+
+@app.websocket("/joy2")
+async def spacemouse_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    global msg2
+    while True:
+        gamepad_info = await websocket.receive_json()
+        for i in range(len(gamepad_info["axes"])):
+            if (len(msg.axes) <= i):
+                msg2.axes.append(gamepad_info["axes"][i])
+            else:
+                msg2.axes[i] = gamepad_info["axes"][i]
+
+        for i in range(len(gamepad_info["buttons"])):
+            if (len(msg2.buttons) <= i):
+                msg2.buttons.append(int(gamepad_info["buttons"][i]))
+            else:
+                msg2.buttons[i] = int(gamepad_info["buttons"][i])
         
 def web_start():
     print("boot webserver thread")
-    uvicorn.run(app, host="127.0.0.1", port=8700)
+    uvicorn.run(app, host="0.0.0.0", port=8700)
 
 def exchangeMapping(mapping):
     # comming soon
@@ -142,18 +163,20 @@ class JoyNodeWeb(Node):
         qos_profile = QoSProfile(depth=10)
         self.timer = self.create_timer(0.05, self.update_joy)
         self.pub = self.create_publisher(Joy, "/joy", qos_profile=qos_profile)
+        self.pub2 = self.create_publisher(Joy, "/joy2", qos_profile=qos_profile)
     
     def update_joy(self):
         global msg;
         msg.header.stamp = self.get_clock().now().to_msg()
         self.pub.publish(msg)
+        self.pub2.publish(msg2)
 
 def main(args = None):
     rclpy.init(args=args)
     thread_web = threading.Thread(target=web_start)
     thread_web.start()
     joy_node = JoyNodeWeb()
-    print("please open http://127.0.0.1:8700/joy")
+    print("please open domain:8700/joy")
     rclpy.spin(joy_node)
 
 if __name__ == '__main__':
